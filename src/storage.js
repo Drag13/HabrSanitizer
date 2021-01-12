ae89e37eccc8b2a35a6d8c04523f12d7be4864a4
Vitalii Drag <ruban.vitaliy@gmail.com>
2021-01-12 22:56:13 +0200

    Added eslint rules Fixed eslint violations Added eslint to prepush hook

--- src/storage.js
+++ src/storage.js
@@ -1,11 +1,46 @@
-export class Storage {
-    _handler;
+/** @typedef {{banned:Array<{name:string, disabled: boolean}>,isQuickActionsOn: boolean}} SanitizerSettings */
 
+/**
+ * Check equality for nullable string ignoring case
+ * @param {string} v1 First string
+ * @param {string} v2 Second string
+ * @return {boolean} Is strings equals ignoring case
+ */
+const isEquals = (v1, v2) => (v1 || '').toString().toLowerCase() === (v2 || '').toString().toLowerCase();
+
+/**
+ * Checks if string is null or empty
+ * @param {string} v String to check
+ * @return {boolean} is string null or empty
+ */
+const isNull = (v) => v == null || v === '';
+
+/**
+ * Persistent storage for the user settings
+ */
+export class Storage {
+    /**
+     * Returns instance of the storage
+     */
+    constructor() {
+        this._notify = (settings) => {
+            typeof this._handler === 'function' && this._handler(settings);
+        };
+    }
+
+    /**
+     * Gets settings from the Storage
+     * @return {SanitizerSettings}
+     */
     async getSettings() {
         const settings = await this._loadSettings();
         return settings;
     }
 
+    /**
+     * Push new settings to the storage (overrides all)
+     * @param {SanitizerSettings} settings
+     */
     async applySettings(settings) {
         if (!settings || !settings.banned || !Array.isArray(settings.banned)) {
             throw new Error('Settings formatted incorrectly');
@@ -24,7 +59,7 @@ export class Storage {
         }
 
         const settings = await this._loadSettings();
-        const exists = !!settings.banned.find((x) => this._eq(x.name, author.name));
+        const exists = !!settings.banned.find((x) => isEquals(x.name, author.name));
 
         if (exists) {
             return false;
@@ -42,12 +77,12 @@ export class Storage {
      * @param {string} userName name of the author
      */
     async removeFromBan(userName) {
-        if (this._null(userName)) {
+        if (isNull(userName)) {
             return;
         }
 
         const settings = await this._loadSettings();
-        settings.banned = settings.banned.filter((author) => !this._eq(author.name, userName));
+        settings.banned = settings.banned.filter((author) => !isEquals(author.name, userName));
 
         await this._saveSettings(settings);
         this._notify(settings);
@@ -64,7 +99,15 @@ export class Storage {
     }
 
     /**
-     * @returns {Promise<{banned:[], isQuickActionsOn:boolean, isPopularIgnored:boolean>}
+     * Subscribe to settings change
+     * @param {Function} handler
+     */
+    onChange(handler) {
+        this._handler = handler;
+    }
+
+    /** Private
+     * @return {SanitizerSettings} user settings
      */
     async _loadSettings() {
         return new Promise((res, _) => {
@@ -72,25 +115,13 @@ export class Storage {
         });
     }
 
+    /**
+     * Private
+     * @param {SanitizerSettings} settings
+     */
     async _saveSettings(settings) {
         return new Promise((res, _) => {
             chrome.storage.sync.set({ settings }, res);
         });
     }
-
-    /**
-     * Subscribe to settings change
-     * @param {(settings)=> void} handler
-     */
-    onChange(handler) {
-        this._handler = handler;
-    }
-
-    _notify = (settings) => {
-        typeof this._handler === 'function' && this._handler(settings);
-    };
-
-    _eq = (v1, v2) => (v1 || '').toString().toLowerCase() === (v2 || '').toString().toLowerCase();
-
-    _null = (v) => v == null || v === '';
 }
